f37b8f7ba683f9da022ce294fbf9763d470b98fd
sjaymoon15 <sjaymoon15@gmail.com>
2017-11-24 16:58:58 -0800

    [feature] add employee model

---
+++ models/employee.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EmployeeSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  job_title: {
+    type: String
+  },
+  company: {
+    type: Schema.Types.ObjectId, ref: 'company'
+  },
+  favorite_drink: {
+    type: Schema.Types.ObjectId, ref: 'drink'
+  }
+});
+
+const Employee = mongoose.model('employee', EmployeeSchema);
+
+module.exports = Employee;
