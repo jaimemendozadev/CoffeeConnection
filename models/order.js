29dbcacd9e224add92a1a60ed501c4e78748918c
sjaymoon15 <sjaymoon15@gmail.com>
2017-11-27 22:12:56 -0800

    [feature] add a order model

---
+++ models/order.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+  ordered_by: {
+    type: Schema.types.ObjectId,
+    ref: 'employee'
+  },
+  drinks: [{
+    type: Schema.types.ObjectId,
+    ref: 'drink'
+  }]
+})
+
+const Order = mongoose.model('order', OrderSchema);
+
+module.exports = Order;
