6c8bb1e47e1c2d1fd209b56132e14762f14d5091
vvnsze <vvnsze@gmail.com>
2017-12-02 22:57:21 -0800

    [setup] add react express and graphql packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const path = require('path');
+
 module.exports = {
   entry: './client/index.js',
   output: {
@@ -9,5 +12,8 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [
+  new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
 };
