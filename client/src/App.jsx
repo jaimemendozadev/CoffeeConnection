3ba453a2c5191a9f87f08ea204b42b0743383d8f
sjaymoon15 <sjaymoon15@gmail.com>
2017-12-06 18:13:19 -0800

    [fix] remove search component import and link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
-import Search from './Search';
 
 const FourOhFour = () => <h1>Uh Oh! 404 Page Not Found. Something happens!</h1>;
 
