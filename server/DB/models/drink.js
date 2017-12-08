2a4d43da8a11b99e95b55ce59626dda9a46aa26a
sjaymoon15 <sjaymoon15@gmail.com>
2017-12-08 05:41:49 -0800

    [refactor] update drink schema

--- server/DB/models/drink.js
+++ server/DB/models/drink.js
@@ -6,39 +6,16 @@ const DrinkSchema = new Schema({
     type: String,
     required: true
   },
-  size: [],
   store: {
-    type: String
+      type: Schema.Types.ObjectId,
+      ref: 'store'
   },
-  ingredients: [],
+  available_sizes: [],
+  milk_options: [],
   image_url: {
-    type: String
-  },
-  options: {
     type: String
   }
 });
-
-/**********************************************
- *Temporarily amending schema for simplification
- ***********************************************
-  size: {
-    type: String
-  },
-
-  ingredients: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'ingredient'
-    }
-  ],
-  store: {
-    type: Schema.Types.ObjectId,
-    ref: 'store'
-  },
-*/
-
-
 
 const Drink = mongoose.model('drink', DrinkSchema);
 
