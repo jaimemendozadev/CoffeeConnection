5acc2b2bd0b3c87cab4f8610089ecebc7f261c05
sjaymoon15 <sjaymoon15@gmail.com>
2017-12-09 19:26:39 -0800

    [refactor] update employee schema

--- server/DB/models/employee.js
+++ server/DB/models/employee.js
@@ -4,24 +4,33 @@ const Schema = mongoose.Schema;
 const EmployeeSchema = new Schema({
   first_name: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   last_name: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
     type: String,
     required: true
   },
   job_title: {
     type: String
   },
-  isBeverageFetcher: {
+  is_beverage_fetcher: {
     type: Boolean,
     default: false
   },
-  isAdmin: {
+  is_admin: {
     type: Boolean,
     default: false
   },
@@ -38,7 +47,22 @@ const EmployeeSchema = new Schema({
       type: Schema.Types.ObjectId,
       ref: 'order'
     }
-  ]
+  ],
+  updated_at: Date,
+  created_at: Date
+}, { runSettersOnQuery: true });
+
+EmployeeSchema.virtual('full_name').get(function() {
+  return `${this.first_name} ${this.last_name}`;
+});
+
+EmployeeSchema.pre('save', function(next) {
+  var currentDate = new Date();
+  this.updated_at = currentDate;
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
+  next();
 });
 
 const Employee = mongoose.model('employee', EmployeeSchema);
