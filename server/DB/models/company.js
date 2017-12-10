548548cab38a9ef92ffcdb2ff00ee0872a69e23d
sjaymoon15 <sjaymoon15@gmail.com>
2017-12-09 19:26:11 -0800

    [refactor] update company schema

--- server/DB/models/company.js
+++ server/DB/models/company.js
@@ -4,14 +4,20 @@ const Schema = mongoose.Schema;
 const CompanySchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    text: true,
+    trim: true
   },
   employees: [
     {
       type: Schema.Types.ObjectId,
       ref: 'employee'
     }
-  ]
+  ],
+  beverage_fetcher: {
+    type: Schema.Types.ObjectId,
+    ref: 'employee'
+  }
 });
 
 const Company = mongoose.model('company', CompanySchema);
