c90b1f28c4c48a194b4551b540b849f143c4de1c
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2017-12-22 12:53:27 -0800

    undo app and landing changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,6 @@ import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 
-import Employees from './components/Employees';
-import Home from './components/Home';
-import Public from './components/Public';
-import User from './components/User';
-
 const FourOhFour = () => <h1>Uh Oh! 404 Page Not Found. Something happens!</h1>;
 
 const App = () => (
@@ -15,10 +10,6 @@ const App = () => (
     <div className="app">
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/employees" component={Employees} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/public" component={Public} />
-        <Route exact path="/user" component={User} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
