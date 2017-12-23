9a1aac8d6ed21b6b3b6c7584048f44cf4e64ce1a
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2017-12-22 16:33:48 -0800

    Fix changes to App and Landing

--- client/src/Landing.jsx
+++ client/src/Landing.jsx
@@ -8,7 +8,7 @@ export default class LandingPage extends Component {
       <div className="text-center">
         <h1>CoffeeConnection</h1>
         <div>
-          <Link to='components/home'><Button className="btn btn-lg" bsStyle="success">Get Started</Button></Link>
+          <Link to='/home'><Button className="btn btn-lg" bsStyle="success">Get Started</Button></Link>
         </div>
         <Image src="https://i.pinimg.com/originals/8c/6c/55/8c6c55cd70c5dc9bac7860b8d89c386c.gif" />
       </div>
