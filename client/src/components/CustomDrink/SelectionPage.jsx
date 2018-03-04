2c19fac8265d07c0792d29416d240aec1c4905d4
jaimemendozadev <jaimemendozadev@gmail.com>
2018-03-04 14:05:38 -0800

    [fix] amend package.json for linter to ignore test dir

--- client/src/components/CustomDrink/SelectionPage.jsx
+++ client/src/components/CustomDrink/SelectionPage.jsx
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 
 class SelectionPage extends Component {
-  render() {
+  render () {
     return (
       <div>SelectionPage</div>
     )
